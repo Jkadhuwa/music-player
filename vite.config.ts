f91b739dbfb82e1e23188bd497507fe61bc4ea86
Jkadhuwa <justinemsinda@gmail.com>
2023-12-04 10:38:08 +0300

    ch-(Setup): Add core packages
    [Finishes AddingCoreDep]

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-
+import path from 'path';
 import react from '@vitejs/plugin-react';
+
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
